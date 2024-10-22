1806f85d487b1dd5ca829295e503b587a395e860
nagisreader <chrisgab005@alpinesd.org>
2024-10-22 09:20:13 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,11 @@
 #while summation code following directions from Readme
+user_num = int(input("Enter a number: "))
+x = 0
+times_looped = 0
+total = 0
+
+while x <= user_num and times_looped < user_num:
+    total += x
+    x += 1
+    
+print(total)
