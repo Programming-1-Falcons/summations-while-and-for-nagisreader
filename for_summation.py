268e7c1f5846b8229a5b5da0afbc4bdb0fb2b428
nagisreader <chrisgab005@alpinesd.org>
2024-10-22 09:16:20 -0600

    added code for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,1 +1,11 @@
 #for Summation code here
+user_num = int(input("Enter a number: "))
+x = 0
+times_looped = 0
+total = 0
+
+while x <= user_num and times_looped < user_num:
+    total += x
+    x += 1
+    
+print(total)
